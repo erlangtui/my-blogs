6d3290232f8acff3a5873a3c7ed70d79eb0e506e
zhangliang <1364255742@qq.com>
2023-08-06 23:39:13 +0800

    update time

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -84,9 +84,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         text: '索引',
         link: '/archives/',
         items: [
+          { text: '归档', link: '/archives/' },
           { text: '分类', link: '/categories/' },
           { text: '标签', link: '/tags/' },
-          { text: '归档', link: '/archives/' },
         ],
       },
       { 
