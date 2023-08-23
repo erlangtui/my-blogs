8173ec5e2ca1c43edb3fbe59e5518beea64371f8
zhangliang <1364255742@qq.com>
2023-08-23 22:39:53 +0800

    update net and consul

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ import dayjs from 'dayjs'
 import baiduCode from './config/baiduCode' // 百度统计hm码
 import htmlModules from './config/htmlModules' // 自定义插入的html块
 
-const DOMAIN_NAME = 'erlangtui.com' // 域名 (不带https)
+const DOMAIN_NAME = 'erlangtui.top' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
