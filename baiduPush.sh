0859cea1745eaa43c4bf6979b39debfbbdccb851
zhangliang <1364255742@qq.com>
2023-12-19 18:59:33 +0800

    update

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=erlangtui.top&token=NHiYk6g2KD2tUmk6"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://erlangtui.top&token=NHiYk6g2KD2tUmk6"
 
 rm -rf urls.txt # 删除文件
